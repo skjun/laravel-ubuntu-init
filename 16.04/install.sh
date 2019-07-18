9744851ac15483fd0197142e70438f8af9cca456
skjun <skjun.java@gmail.com>
2019-07-18 16:24:05 +0800

    Update install.sh

--- 16.04/install.sh
+++ 16.04/install.sh
@@ -77,7 +77,7 @@ call_function install_basic_softwares "正在安装基础软件" ${LOG_PATH}
 call_function install_php "正在安装 PHP" ${LOG_PATH}
 call_function install_others "正在安装 Mysql / Nginx / Redis / Memcached / Beanstalkd / Sqlite3" ${LOG_PATH}
 call_function install_node_yarn "正在安装 Nodejs / Yarn" ${LOG_PATH}
-call_function install_composer "正在安装 Composer" ${LOG_PATH}
+#call_function install_composer "正在安装 Composer" ${LOG_PATH}
 
 ansi --green --bold -n "安装完毕"
 ansi --green --bold "Mysql root 密码："; ansi -n --bold --bg-yellow --black ${MYSQL_ROOT_PASSWORD}
